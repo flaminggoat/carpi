9ef6f0f8c03589eedbb2fe178ff284d76f315827
Theo Hussey <husseytg@gmail.com>
2017-06-12 23:03:28 +0000

    added screen manager to switch between music and menu

--- CarApp.py
+++ CarApp.py
@@ -6,25 +6,34 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
 import time
 
-#Create screen manager
-sm = ScreenManager()
+#Builder.load_file("car.kv")
 
-class MainScreen(Screen):
+class RootWidget(Widget):
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update, 1.0/60.0)
     def update(self, dt):
         clock = self.ids.clock
         clock.text = time.strftime('%I:%M %d/%m/%Y',time.localtime())
+    def setScreen(self, newScreen):
+        self.ids.screen_manager.current = newScreen
 
 class MusicScreen(Screen):
     pass
 
+class MenuScreen(Screen):
+    pass
+
 class CarApp(App):
     def build(self):
-        m = MainScreen()
-        Clock.schedule_interval(m.update, 1.0/60.0)
-        return m
-    
+        self.root = RootWidget()
+        #Clock.schedule_interval(m.update, 1.0/60.0)
+        #sm.current = 'music'
+        return self.root
+   
 if __name__ == '__main__':
     CarApp().run()
